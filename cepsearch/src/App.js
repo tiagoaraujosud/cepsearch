6c23cef76ad0cd467ff7e40e33de3f532ed21de9
tiagoaraujosud <tiagoaraujosud@gmail.com>
2024-05-14 10:11:43 -0300

    Main and CSS style

--- cepsearch/src/App.js
+++ cepsearch/src/App.js
@@ -1,7 +1,31 @@
+import { FiSearch } from 'react-icons/fi'
+import './styles.css';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
+    <div className="container">
+      <h1 className="title">Zip Code</h1>
+
+      <div className='containerInput'>
+        <input
+          type="text"
+          placeholder="Type your Zip code(Brazil only)"
+        />
+
+        <button className='buttonSearch'>
+          <FiSearch size={25} color='#FFF'/>
+        </button>
+      </div>
+
+      <main className='main'>
+        <h2>Zip Code: 89651321</h2>
+
+        <span>Street Test</span>
+        <span>Complement: Some Complement</span>
+        <span>Utah</span>
+        <span>Oren</span>
+      </main>
+
     </div>
   );
 }
