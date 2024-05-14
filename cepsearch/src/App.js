8eff84b1a63fd902c2596b1e41b3fd939b66c9f9
tiagoaraujosud <tiagoaraujosud@gmail.com>
2024-05-14 11:41:41 -0300

    Functions

--- cepsearch/src/App.js
+++ cepsearch/src/App.js
@@ -1,7 +1,32 @@
-import { FiSearch } from 'react-icons/fi'
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
 import './styles.css';
 
+import api from './services/api';
+
 function App() {
+
+  const [input, setInput] = useState('')
+  const [zip, setZip] = useState({})
+
+  async function handleSearch() {
+    
+    if(input === '') {
+      alert('Nothing was typed!')
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`)
+      setZip(response.data);
+      console.log(zip)
+      setInput("")
+    } catch (error) {
+      alert('Error')
+      setInput("")
+    }
+  }
+
   return (
     <div className="container">
       <h1 className="title">Zip Code</h1>
@@ -10,22 +35,25 @@ function App() {
         <input
           type="text"
           placeholder="Type your Zip code(Brazil only)"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
         />
 
-        <button className='buttonSearch'>
+        <button className='buttonSearch'onClick={handleSearch}>
           <FiSearch size={25} color='#FFF'/>
         </button>
       </div>
 
-      <main className='main'>
-        <h2>Zip Code: 89651321</h2>
+      {Object.keys(zip).length > 0 && (
+        <main className='main'>
+        <h2>Zip Code: {zip.cep}</h2>
 
-        <span>Street Test</span>
-        <span>Complement: Some Complement</span>
-        <span>Utah</span>
-        <span>Oren</span>
+        <span>{zip.logradouro}</span>
+        <span>Complement: {zip.complemento}</span>
+        <span>{zip.bairro}</span>
+        <span>{zip.localidade} - {zip.uf}</span>
       </main>
-
+      )}
     </div>
   );
 }
